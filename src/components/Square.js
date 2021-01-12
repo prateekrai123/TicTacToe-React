8662af68955bb9c85b12c5743eed10a3bc79716c
Prateek <rprateek254@gmail.com>
2021-01-12 21:10:18 +0530

    styling with many css files

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import './Square.style.css';
 
 const Square = ({ value }) => {
-  return <button type="button">{value + 1}</button>;
+  return (
+    <button type="button" className="btn">
+      {value + 1}
+    </button>
+  );
 };
 
 export default Square;
